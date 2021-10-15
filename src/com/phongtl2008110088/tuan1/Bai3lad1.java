9876bcf35d6c853fdc05f98c7d9b73c21fdbedd4
phongtl2008110088 <taurus15052002@gmail.com>
2021-10-16 01:50:47 +0700

    sửa bài tập

--- src/com/phongtl2008110088/tuan1/Bai3lad1.java
+++ src/com/phongtl2008110088/tuan1/Bai3lad1.java
@@ -8,11 +8,11 @@ public class Bai3lad1 {
         double delta;
         int a, b, c;
         Scanner heSo = new Scanner(System.in);
-        System.out.print("Nhập a = ");
+        System.out.print("Nhập số thứ nhất = ");
         a = heSo.nextInt();
-        System.out.print("Nhập b = ");
+        System.out.print("Nhập số thứ hai = ");
         b = heSo.nextInt();
-        System.out.print("Nhập c = ");
+        System.out.print("Nhập sô thứ ba = ");
         c = heSo.nextInt();
         delta = Math.pow(b, 2) - 4*a*c;
         System.out.printf("Delta = %.2f", delta);
--- src/com/phongtl2008110088/tuan3/Bai1lad3.java
+++ src/com/phongtl2008110088/tuan3/Bai1lad3.java
@@ -7,7 +7,7 @@ public class Bai1lad3 {
         int n;
         boolean kiemTra = true;
         Scanner N = new Scanner(System.in);
-        System.out.print("Nhập số n = ");
+        System.out.print("Nhập số bất kì = ");
         n = N.nextInt();
         for(int i = 2;i < n - 1;i++){
             if(n % i == 0){
