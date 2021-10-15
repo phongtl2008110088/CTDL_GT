fc24f0fb9095ce4fe646af3a67cc645a1b3509ac
phongtl2008110088 <taurus15052002@gmail.com>
2021-10-16 01:43:27 +0700

    sửa lại bai4lad2

--- src/com/phongtl2008110088/tuan2/Bai4lad2.java
+++ src/com/phongtl2008110088/tuan2/Bai4lad2.java
@@ -7,9 +7,9 @@ public class Bai4lad2 {
         int d;
         Scanner luaChon = new Scanner(System.in);
         System.out.println("----------MENU----------");
-        System.out.println(">>1: Giải phương trình bậc 1.");
-        System.out.println(">>2: Giải phương trình bậc 2.");
-        System.out.println(">>3: Tính tiền điện.");
+        System.out.println("1: Giải phương trình bậc 1.");
+        System.out.println("2: Giải phương trình bậc 2.");
+        System.out.println("3: Tính tiền điện.");
         System.out.print("Nhập vào sự lựa chọn(1>>3): " );
         d = luaChon.nextInt();
         switch(d) {
@@ -23,9 +23,9 @@ public class Bai4lad2 {
 
     static void giaiPTBac1() {
         Scanner bien = new Scanner(System.in);
-        System.out.print("Nhập a = ");
+        System.out.print("Nhập a : ");
         int a = bien.nextInt();
-        System.out.print("Nhập b = ");
+        System.out.print("Nhập b : ");
         int b = bien.nextInt();
         if(a == 0){
             if(b == 0)
@@ -42,11 +42,11 @@ public class Bai4lad2 {
     static void giaiPTBac2() {
         double x1, x2, delta;
         Scanner heSo = new Scanner(System.in);
-        System.out.print("Nhập a = ");
+        System.out.print("Nhập a : ");
         int a = heSo.nextInt();
-        System.out.print("Nhập b = ");
+        System.out.print("Nhập b : ");
         int b = heSo.nextInt();
-        System.out.print("Nhập c = ");
+        System.out.print("Nhập c : ");
         int c = heSo.nextInt();
         if(a == 0){
             if(b == 0){
