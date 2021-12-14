322ab97b6a13512387457498c8839c2eb9da1306
phongtl2008110088 <92381203+phongtl2008110088@users.noreply.github.com>
2021-12-14 10:56:54 +0700

    Delete src/com directory

--- src/com/phongtl2008110088/kiemtragiuaki/LinkedListtestdiver.java
+++
@@ -1,8 +0,0 @@
-package com.phongtl2008110088.kiemtragiuaki;
-
-public class LinkedListtestdiver {
-    public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-        list.addFirst();
-    }
-}
