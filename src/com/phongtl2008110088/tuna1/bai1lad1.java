1a2699ea56c500beabfaddcf13d6727e83154c10
phongtl2008110088 <taurus15052002@gmail.com>
2021-12-14 11:27:24 +0700

    up lai may bai tap bi xoa

---
+++ src/com/phongtl2008110088/tuna1/bai1lad1.java
@@ -0,0 +1,17 @@
+package com.phongtl2008110088.tuna1;
+
+import java.util.Scanner;
+
+public class bai1lad1 {
+    public static void main(String[] args) {
+        String hoVaTen;
+        Double diemTB;
+        Scanner thongTin = new Scanner(System.in);
+        System.out.print("Họ và tên: ");
+        hoVaTen = thongTin.nextLine();
+        System.out.print("Điểm trung bình: ");
+        diemTB = thongTin.nextDouble();
+        System.out.printf("%s: %.2f điểm", hoVaTen, diemTB);
+        thongTin.close();
+    }
+}
